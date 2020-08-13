bb28ea4fe5dc89dea76a5890c539f30136206334
Vikra Ardiansyah <vikraardi@gmail.com>
2020-08-14 01:10:44 +0700

    get data

--- src/App.js
+++ src/App.js
@@ -1,57 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import {
+  Container,
+  Row,
+  Col,
   Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
+  Input,
 } from "reactstrap";
+import axios from "axios";
+import Card from "./components/Card";
 
-const Example = (props) => {
+const App = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => {
+        setData(response.data.results);
+        // response.data.results.map((result) =>
+        //   axios.get(`${result.url}`).then((response) => {
+        //     let temp = data;
+        //     temp.push(response.data);
+        //     setData(temp);
+        //   })
+        // );
+      })
+      .catch((error) => console.log(error));
+  }, [data]);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
+    <>
+      <Navbar className="navbar-bg" light expand="sm">
+        <Container fluid="xl">
+          <NavbarBrand href="#" className="navbar-text">
+            Pokemon Deck
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar></Nav>
+            <Nav navbar>
+              <Input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search pokemon"
+                autoComplete="off"
+                className="my-2 my-lg-0 rounded-pill search-bg"
+              />
+            </Nav>
+          </Collapse>
+        </Container>
       </Navbar>
-    </div>
+      <Container fluid="xl" className="mt-3">
+        <Row>
+          {data.map((d) => {
+            return (
+              <Col key={d.id} xs="6" sm="6" md="3">
+                <Card name={d.name} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
   );
 };
 
-export default Example;
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { Card, CardImg, CardTitle } from "reactstrap";
+import axios from "axios";
+
+const Cards = ({ name }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((response) =>
+        setData(response.data.sprites.other.dream_world.front_default)
+      );
+  }, [name]);
+
+  return (
+    <div>
+      <Card className="card my-3">
+        <CardImg top src={data} alt="Card image cap" className="img-card" />
+        <CardTitle className="text-center">{name}</CardTitle>
+      </Card>
+    </div>
+  );
+};
+
+export default Cards;
